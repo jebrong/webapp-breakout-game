c492ed6b920461e5894cd09e4a8ca5198f5abf15
jebrong <92987206+jebrong@users.noreply.github.com>
2022-11-08 17:47:51 +0800

    create canvas graphics

--- script.js
+++ script.js
@@ -2,10 +2,94 @@ const rulesBtn = document.querySelector("#rules-btn");
 const closeBtn = document.querySelector("#close-btn");
 const rules = document.querySelector(".rules");
 
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+let score = 0;
+
+const brickRowCount = 9;
+const brockColumnCount = 5;
+
 rulesBtn.addEventListener("click", () => {
   rules.classList.add("show");
 });
-
 closeBtn.addEventListener("click", () => {
   rules.classList.remove("show");
 });
+
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brockColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4,
+};
+const drawBall = () => {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = "#9C4035";
+  ctx.fill();
+  ctx.closePath();
+};
+
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0,
+};
+const drawPath = () => {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = "#3A5C43";
+  ctx.fill();
+  ctx.closePath();
+};
+
+const drawScore = () => {
+  ctx.font = "20px Arial";
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+};
+
+const drawBricks = () => {
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? "#385D65" : "transparent";
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+};
+
+const draw = () => {
+  drawBall();
+  drawPath();
+  drawScore();
+  drawBricks();
+};
+
+draw();
