dfb1d353e0bba067a19c0cec2ea001a4cdff6cf9
jebrong <92987206+jebrong@users.noreply.github.com>
2022-11-08 18:09:50 +0800

    add moving ball and breaking bricks

--- script.js
+++ script.js
@@ -89,6 +89,42 @@ const movePaddle = () => {
   }
 };
 
+const moveBall = () => {
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+    ball.dx *= -1;
+  }
+
+  if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+    ball.dy *= -1;
+  }
+
+  if (
+    ball.x - ball.size > paddle.x &&
+    ball.x + ball.size < paddle.x + paddle.w &&
+    ball.y + ball.size > paddle.y
+  ) {
+    ball.dy = -ball.speed;
+  }
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      if (brick.visible) {
+        if (
+          ball.x - ball.size > brick.x &&
+          ball.x + ball.size < brick.x + brick.w &&
+          ball.y + ball.size > brick.y &&
+          ball.y - ball.size < brick.y + brick.h
+        ) {
+          ball.dy *= -1;
+          brick.visible = false;
+        }
+      }
+    });
+  });
+};
+
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
@@ -98,6 +134,7 @@ const draw = () => {
 };
 
 const update = () => {
+  moveBall();
   movePaddle();
   draw();
   requestAnimationFrame(update);
